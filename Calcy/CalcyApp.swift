1cbd4d17a204c82f8b78fc5d66cf9c9da8bc9f36
Louis Farmer <louisfarmer@live.co.uk>
2024-02-16 14:29:26 -0800

    Initial Commit

---
+++ Calcy/CalcyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcyApp.swift
+//  Calcy
+//
+//  Created by Louis Farmer on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CalcyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
